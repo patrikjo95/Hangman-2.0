ef7859f8fcd151bff74dea6cc14cbab2bbfd96bf
Lucas Åkerlund <lucas2001@hotmail.se>
2021-10-06 11:33:03 +0200

    updated everything

--- src/hangman/Game.java
+++ src/hangman/Game.java
@@ -2,9 +2,32 @@ package hangman;
 
 public class Game {
 
-    public Game(){
-        
+    /*
+    * Att göra:
+    * Skapa en randomizer som slumpar ett ord
+    * fixa bokstavsräknaren för ordet
+    * mata ut antal fel gissningar
+    * ta in input från spelare och hantera det
+    * fixa en updateArray metod i Menu för att kunna få de senaste värdena till arrayen.
+    *
+    * */
 
+    private String[] gameMenuArray;
+    private Menu gameMenu;
+    private Player player;
+    public Game(Player player){
+        this.player = player;
+
+        gameMenuArray = new String[]{
+                "Hej " + player.getPlayerName() + "!" + " jag tänker på ett ord som har: " /* + Randomord*/ + "bokstäver",
+                "Så här långt har du gissat: "  /* + bokstavsräknare*/,
+                "Du har gissat på följande bokstäver: " /* + gissade bokstäver*/,
+                "Hängmätare: " /* + antal felaktiga försök*/,
+                "Vilken bokstav vill du gissa på?"
+        };
+
+        gameMenu = new Menu(gameMenuArray);
+        gameMenu.show();
     }
 
     public void showGame() {
@@ -16,12 +39,4 @@ public class Game {
 
    }
 
-
-
-
-
-
-
-
-
-   }
+}
